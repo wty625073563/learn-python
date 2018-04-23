d1b198c37a941a325b7f758a833c564ddfa5ce01
woods <wty625073563@163.com>
2018-04-23 16:31:45 +0800

    基础学习

---
+++ Boolean.py
@@ -0,0 +1,19 @@
+
+# 布尔值 注意大小写   True  False
+
+print(3 > 2)
+
+print(3 < 2)
+
+
+# and(与)  or(或) not(非)
+
+print(True and True, 'and')
+print(True and False, 'and')
+
+print(True or False, 'or')
+print(False or True, 'or')
+print(False or False, 'or')
+
+print(not True, 'not')
+print(not False, 'not')
---
+++ hello.py
@@ -0,0 +1,19 @@
+# print('Hello World')
+
+# name = input('赶紧输入名字：')
+# print('hello', name)
+
+# 转义符
+# 换行
+
+#\n
+print(r'\n')
+print('林\n文第')
+
+# '''...'''
+print('''林
+文第''')
+
+# r''  ''内部不转义
+print('r''  ''内部不转义')
+print('林\n' r'文\n弟')
